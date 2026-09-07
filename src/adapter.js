3a2a815be049b2edc235806e6f916603c5508dad
sebastien michea <smichea@gmail.com>
2026-09-07 15:53:03 +0200

    Prevalidate the packaged game with the official Museum Action

--- src/adapter.js
+++ src/adapter.js
@@ -1,6 +1,6 @@
 import {Werewolf} from './engine.js';
 globalThis.RetroMuseumGame={create(players,saved,options){
  const game=new Werewolf(players,saved,options);
- game.status=()=>({winner:game.winner,requiredPlayers:game.players.filter(p=>p.alive).map(p=>p.id)});
+ game.status=()=>({winner:game.winner,requiredPlayers:game.players.filter(p=>p.alive).map(p=>p.id),decisionSeconds:game.left});
  return game;
 }};
